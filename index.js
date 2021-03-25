2c6d93ca322872bc40db26705410dd96b5dbd536
FreakZombie <wporter82@gmail.com>
2021-03-25 10:01:51 -0700

    Randomize question order

--- index.js
+++ index.js
@@ -179,6 +179,9 @@ async function getQuestions(numQs = 5) {
             }
         }
 
+        shuffleArray(questions);
+
+        // Set the question number "'x' of 'y'"
         let qCount = 1;
         questions.forEach(q => {
             q.question_number = `${qCount++} of ${numQs}`;
@@ -225,4 +228,14 @@ function getWinner() {
 
     return `${index} with ${max} points!`;
     
+}
+
+// Randomize array in-place using Durstenfeld shuffle algorithm
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
